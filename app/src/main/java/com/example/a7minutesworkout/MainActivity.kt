5240372572be6240ceb03cde7861c92120f138f9
Abhinav Yadav <abhinav.yadav.1998@gmail.com>
2022-09-01 21:07:19 +0530

    Adding The Exercise Screen - Intents and Customizing our Toolbar

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -22,6 +23,9 @@ class MainActivity : AppCompatActivity() {
                 "Let's get Fit...",
                 Toast.LENGTH_SHORT
             ).show()
+
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
 
